8ae053b7c1ccd51d2764007dd469bbfc3942aa53
mby444 <bimayudha161@gmail.com>
2025-12-13 22:27:03 +0700

    Fix conflict

--- resources/js/components/product-customer/LocationPage.tsx
+++ resources/js/components/product-customer/LocationPage.tsx
@@ -4,10 +4,6 @@ import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
-<<<<<<< HEAD
-=======
-import { User } from '@/types';
->>>>>>> 9215853eeca0b7224ed41c33b5fe2914f2dfa8a0
 import {
     Briefcase,
     Check,
