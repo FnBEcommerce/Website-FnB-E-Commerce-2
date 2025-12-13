275d1a0a75ddae58552480847e7d996327d401ee
mby444 <bimayudha161@gmail.com>
2025-12-13 12:50:36 +0700

    Add static data into DeliveryController detail

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -13,14 +13,75 @@ use Laravel\Fortify\Features;
 class DeliveryController extends Controller
 {
     public function detail(Order $order) {
+        $user = User::find(2); // TODO: Ubah ketika sudah ada sistem login
         $orderItems = OrderDetail::with('product')
-        ->where('order_id', $order->order_id)
+        ->where('id', $order->id)
         ->get([
-            'order_id',
-            'orderdetail_quantity',
-            'orderdetail_subtotal',
+            'id',
+            'quantity',
+            'subtotal',
             'product_id'
         ]);
-    return Inertia::render('delivery', ['orderNumber' => $order->order_id, 'orderItems' => $orderItems]);
+
+        $nearestBranch = [
+            'name'     => "Cabang Jakarta Selatan (Pusat)",
+            'distance' => "2.3 km",
+            'address'  => "Jl. Senopati No. 10, Jakarta"
+        ];
+
+        $courierInfo = [
+            'name'   => "Budi Santoso",
+            'plate'  => "B 1234 XYZ",
+            'status' => "Menuju lokasi restoran"
+        ];
+
+        $recommendations = [
+            [
+                'id'       => 101,
+                'name'     => "Ice Matcha Latte",
+                'discount' => 0.2,
+                'price'    => 24000
+            ],
+            [
+                'id'       => 102,
+                'name'     => "Choco Croissant",
+                'discount' => 0.15,
+                'price'    => 18000
+            ]
+        ];
+
+        $currentStepIndex = 2; // 0: Confirmed, 1: Processing, 2: Shipped, 3: Delivered
+
+        $trackingDetails = [
+            [
+                'date'      => '12:00 PM',
+                'completed' => true,
+            ],
+            [
+                'date'      => '12:30 PM',
+                'completed' => true,
+            ],
+            [
+                'date'      => 'Sedang berlangsung',
+                'completed' => true,
+            ],
+            [
+                'date'      => 'Est. 30 min',
+                'completed' => false,
+            ],
+        ];
+
+        $props = [
+            'user' => $user,
+            'orderNumber' => $order->id,
+            'orderItems' => $orderItems,
+            'nearestBranch' => $nearestBranch,
+            'courierInfo' => $courierInfo,
+            'recommendations' => $recommendations,
+            'currentStepIndex' => $currentStepIndex,
+            'trackingDetails' => $trackingDetails,
+        ];
+
+        return Inertia::render('delivery', $props);
     }
 }
