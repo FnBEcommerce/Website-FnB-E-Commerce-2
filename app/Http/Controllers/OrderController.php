ff07081794192dc67b608fe77af5e2588eef1394
mby444 <bimayudha161@gmail.com>
2025-12-15 17:00:14 +0700

    Modify OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,14 +8,48 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
+use App\Models\OrderDetail;
+use Illuminate\Support\Facades\Auth;
+
 class OrderController extends Controller
 {
     public function create(Request $request) {
+        // $request->validate([
+        //     'products' => 'required|array',
+        //     'products.*.id' => 'required|exists:products,id',
+        //     'products.*.quantity' => 'required|integer|min:1',
+        // ]);
 
-        $order = Order::create([]);
+        // $user = Auth::user();
 
-        return response()->json([
-            'order' => $order,
-        ]);
+        // $order = Order::create([
+        //     'user_id' => $user->id,
+        //     'order_number' => Str::uuid(),
+        //     'status' => 'created',
+        //     'total_price' => 0,
+        // ]);
+
+        // $totalPrice = 0;
+
+        // foreach ($request->products as $productData) {
+        //     $product = Product::find($productData['id']);
+        //     $quantity = $productData['quantity'];
+        //     $subtotal = $product->price * $quantity;
+
+        //     OrderDetail::create([
+        //         'order_id' => $order->id,
+        //         'product_id' => $product->id,
+        //         'quantity' => $quantity,
+        //         'subtotal' => $subtotal,
+        //     ]);
+
+        //     $totalPrice += $subtotal;
+        // }
+
+        // $order->update(['total_price' => $totalPrice]);
+
+        // return response()->json([
+        //     'order' => $order->load('details.product'),
+        // ]);
     }
 }
--- resources/js/features/admin/index.tsx
+++ resources/js/features/admin/index.tsx
@@ -46,8 +46,72 @@ const topNav = [
     },
 ];
 
+// TODO pindah ini
+export type StatsData = {
+    title: string;
+    value: string;
+    change: string;
+    icon: React.ElementType;
+};
+
+// export type
+
 export function AdminDashboard() {
     const [activeTab, setActiveTab] = useState('overview');
+
+    const statsData = [];
+    const overviewData = [
+        {
+            name: 'Jan',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Feb',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Mar',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Apr',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'May',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Jun',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Jul',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Aug',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Sep',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Oct',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Nov',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: 'Dec',
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+    ];
+
+    const recentSales = [];
 
     return (
         <>
@@ -218,7 +282,7 @@ export function AdminDashboard() {
                                     <CardTitle>Overview</CardTitle>
                                 </CardHeader>
                                 <CardContent className="ps-2">
-                                    <Overview />
+                                    <Overview data={overviewData} />
                                 </CardContent>
                             </Card>
                             <Card className="col-span-1 lg:col-span-3">
