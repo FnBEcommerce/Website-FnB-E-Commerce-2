722447206e8ccb92b6ee6e07112d3f444c137d58
mby444 <bimayudha161@gmail.com>
2025-12-16 14:28:34 +0700

    Modify seeder, ensure every product belongs to any branches

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,54 +34,63 @@ class DatabaseSeeder extends Seeder
         // Create products
         $products = Product::factory(50)->create();
 
-        // Create shop branches and associate products and users with them
-        ShopBranch::factory(5)
-            ->hasAttached($products->random(10), [], 'products') // Specify the relationship name
-            ->hasAttached($users->random(2), [], 'assignedUsers')      // Specify the relationship name
-            ->create()
-            ->each(function ($shop) use ($users, $couriers, $products) {
-                // For each shop, create a few orders
-                Order::factory(rand(5, 10))
-                    ->for($users->random()->first(), 'user')
-                    ->for($couriers->random()->first(), 'courier')
-                    ->for($shop, 'shopBranch')
-                    ->has(
-                        OrderDetail::factory(rand(1, 4))
-                            ->state(function (array $attributes, Order $order) use ($shop) {
-                                // Get products available only at the order's shop branch
-                                $product = $shop->products->random();
-                                $quantity = rand(1, 3);
-                                return [
-                                    'product_id' => $product->id,
-                                    'quantity' => $quantity,
-                                    'subtotal' => $product->price_discount * $quantity,
-                                ];
-                            })
-                    )
-                    ->create()
-                    ->each(function ($order) {
-                        // After creating order details, update the order totals
-                        $subtotal = $order->orderDetails->sum('subtotal');
-                        $order->update([
-                            'subtotal' => $subtotal,
-                            'total' => $subtotal + $order->delivery_fee,
-                        ]);
+        // Create shop branches
+        $branches = ShopBranch::factory(5)->create();
 
-                        // Create reviews for some of the products in the order
-                        foreach ($order->orderDetails as $detail) {
-                            if (rand(0, 1)) { // 50% chance to review
-                                $reviewDate = (clone $order->confirmed_at)->add(new \DateInterval('P' . rand(1, 7) . 'D'));
-                                Review::factory()
-                                    ->for($order->user)
-                                    ->for($detail->product)
-                                    ->create([
-                                        'created_at' => $reviewDate,
-                                        'updated_at' => $reviewDate,
-                                    ]);
-                            }
+        // Ensure every product is in at least one branch
+        $products->each(function ($product) use ($branches) {
+            $product->shopBranches()->attach(
+                $branches->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
+
+        // Create shop branches and associate products and users with them
+        $branches->each(function ($shop) use ($users, $couriers) {
+            // Assign some users to this branch
+            $shop->assignedUsers()->attach($users->random(2));
+
+            // For each shop, create a few orders
+            Order::factory(rand(5, 10))
+                ->for($users->random()->first(), 'user')
+                ->for($couriers->random()->first(), 'courier')
+                ->for($shop, 'shopBranch')
+                ->has(
+                    OrderDetail::factory(rand(1, 4))
+                        ->state(function (array $attributes, Order $order) use ($shop) {
+                            // Get products available only at the order's shop branch
+                            $product = $shop->products->random();
+                            $quantity = rand(1, 3);
+                            return [
+                                'product_id' => $product->id,
+                                'quantity' => $quantity,
+                                'subtotal' => $product->price_discount * $quantity,
+                            ];
+                        })
+                )
+                ->create()
+                ->each(function ($order) {
+                    // After creating order details, update the order totals
+                    $subtotal = $order->orderDetails->sum('subtotal');
+                    $order->update([
+                        'subtotal' => $subtotal,
+                        'total' => $subtotal + $order->delivery_fee,
+                    ]);
+
+                    // Create reviews for some of the products in the order
+                    foreach ($order->orderDetails as $detail) {
+                        if (rand(0, 1)) { // 50% chance to review
+                            $reviewDate = (clone $order->confirmed_at)->add(new \DateInterval('P' . rand(1, 7) . 'D'));
+                            Review::factory()
+                                ->for($order->user)
+                                ->for($detail->product)
+                                ->create([
+                                    'created_at' => $reviewDate,
+                                    'updated_at' => $reviewDate,
+                                ]);
                         }
-                    });
-            });
+                    }
+                });
+        });
 
         $this->call([
             NotificationSeeder::class,
