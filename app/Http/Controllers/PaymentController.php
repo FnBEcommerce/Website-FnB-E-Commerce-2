750fb678a00c5d56bacc2baed9e500b9441cd005
mby444 <bimayudha161@gmail.com>
2025-12-17 10:39:35 +0700

    Add unifinshed fake payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\User;
 use Midtrans\Snap;
 use Midtrans\Config;
 use Illuminate\Http\Request;
@@ -11,6 +13,45 @@ use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
+    public function create(Request $request)
+    {
+        
+        // return response()->json($request->all());
+        
+        try {
+            $user = Auth::user();
+            // $shop_branch = User::find($user->id)->shopBranches();
+            // return response()->json($shop_branch);
+            $order = Order::create([
+                'user_id' => $user->id,
+                // 'shop_branch_id' => $shop_branch_id,
+                'total' => $request->total,
+                'status' => 'created',
+                'payment_method' => $request->payment_method,
+                'total_amount' => $request->total_amount,
+                'subtotal' => $request->total,
+                'delivery_fee' => $request->delivery_fee,
+            ]);
+            foreach ($request->cart_items as $item) {
+                OrderDetail::create([
+                    'order_id' => $order->id,
+                    'product_id' => $item['product_id'],
+                    'quantity' => $item['quantity'],
+                ]);
+            }
+    
+            return response()->json([
+                'order_id' => $order->id,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Gagal membuat order: ' . $th->getMessage(),
+            ]);
+        }
+        
+
+    }
+
     public function pay(Order $order)
     {
         // VALIDASI
